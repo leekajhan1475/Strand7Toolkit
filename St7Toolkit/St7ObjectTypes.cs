24a39a6a08be57339871a0499fc6ca5f4bee56b6
zonhan1475 <61720983+leekajhan1475@users.noreply.github.com>
2025-05-07 07:58:56 +0800

    Add files via upload

---
+++ St7Toolkit/St7ObjectTypes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using St7Toolkit.Element;
+
+namespace St7Toolkit.Types
+{
+    /// <summary>
+    /// Utility module that caches a number of oft-used types and type-ids for quick
+    /// access. Do not change any of the values in this module or you'll wreck casting
+    /// and conversion logic.
+    /// </summary>
+    public static class St7ObjectTypes
+    {
+        /// <summary>
+        /// Type of St7Toolkit.Element.Node
+        /// </summary>
+        public static Type tNODE = typeof(Node);
+
+        /// <summary>
+        /// Type of St7Toolkit.Element.Beam
+        /// </summary>
+        public static Type tyBEAM = typeof(Beam);
+
+        /// <summary>
+        /// Type of St7Toolkit.Element.Plate
+        /// </summary>
+        public static Type tyPLATE = typeof(Plate);
+
+        /// <summary>
+        /// Type of St7Toolkit.Element.Plate
+        /// </summary>
+        public static Type tyMODEL = typeof(St7Model);
+
+        /// <summary>
+        /// Guid of type St7Toolkit.Element.Node
+        /// </summary>
+        public static Guid idNODE = tNODE.GUID;
+
+        /// <summary>
+        /// Guid of type St7Toolkit.Element.Beam
+        /// </summary>
+        public static Guid idBEAM = tyBEAM.GUID;
+
+        /// <summary>
+        /// Guid of type St7Toolkit.Element.Plate
+        /// </summary>
+        public static Guid idPLATE = tyPLATE.GUID;
+
+        /// <summary>
+        /// Guid of type St7Toolkit.Element.Plate
+        /// </summary>
+        public static Guid idMODEL = tyMODEL.GUID;
+    }
+}
