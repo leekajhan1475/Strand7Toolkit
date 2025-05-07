156cf0637ef7e554dcb024977e38077434fa33c7
zonhan1475 <61720983+leekajhan1475@users.noreply.github.com>
2025-05-07 22:17:45 +0800

    Add files via upload

---
+++ St7Toolkit/St7Toolkit/Model/St7Model_GetMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using St7Toolkit.Element;
+using St7API;
+using static St7Toolkit.St7Model;
+using Rhino;
+
+namespace St7Toolkit
+{
+    /// <summary>
+    /// Implements all of the Strand7 API methods.
+    /// </summary>
+    public partial class St7Model
+    {
+        
+
+    }
+}
