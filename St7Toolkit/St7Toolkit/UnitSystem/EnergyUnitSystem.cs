ccf11443c5702c5f447bac433234cc6e285c218f
zonhan1475 <61720983+leekajhan1475@users.noreply.github.com>
2025-05-08 18:00:07 +0800

    Add files via upload

---
+++ St7Toolkit/St7Toolkit/UnitSystem/EnergyUnitSystem.cs
@@ -0,0 +1,47 @@
+﻿using St7API;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace St7Toolkit
+{
+    /// <summary>
+    /// Measuring energy in units of joules, kilojoules, British thermal units, foot pounds-force or calories respectively.
+    /// </summary>
+    public enum EnergyUnitSystem
+    {
+        /// <summary>
+        /// No length unit system is specified and model length unit system should be used.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Joules
+        /// </summary>
+        J = St7.euJOULE,
+
+        /// <summary>
+        /// Kilo-joules
+        /// </summary>
+        Kj = St7.euKILOJOULE,
+
+        /// <summary>
+        /// British thermal unit
+        /// </summary>
+        Btu = St7.euBTU,
+
+        /// <summary>
+        /// Foot pounds-force(lbf)
+        /// </summary>
+        FtLbF = St7.euFTLBF,
+
+        /// <summary>
+        /// Calories
+        /// </summary>
+        Cal = St7.euCALORIE,
+
+        /// <summary>
+        /// No unit system is set
+        /// </summary>
+        Unset = 255
+    }
+}
